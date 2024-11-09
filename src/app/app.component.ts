bbf7e8e7d53b26bbc70b4e265543ea5bef33aea8
Yuvaraj456 <yuvarajryd1@gmail.com>
2024-11-09 22:56:04 +0530

    package update 1

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,7 +1,7 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterLink, RouterOutlet } from '@angular/router';
-import { HttpClientModule } from '@angular/common/http';
+import {} from '@angular/common/http';
 
 @Component({
   selector: 'app-root',
